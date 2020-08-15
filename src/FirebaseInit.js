68dcff04a74c49deaeb8469cf197b9af36004961
bslzz <bishalkoid@gmail.com>
2020-08-15 14:27:03 +0300

    added config file to secure data

--- src/FirebaseInit.js
+++ src/FirebaseInit.js
@@ -1,14 +1,6 @@
 import Firebase from 'firebase';
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DB_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
+import { FIREBASE_CONFIG as firebaseConfig } from './Components/Config/FirebaseConfig';
+
 // Initialize Firebase
 Firebase.initializeApp(firebaseConfig);
 
